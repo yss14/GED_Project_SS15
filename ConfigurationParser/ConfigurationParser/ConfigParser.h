321c1321140843d771d37812ad4193939a3f80ae
yss14 <yannickstach@googlemail.com>
2015-04-28 14:43:00 +0200

    Recreated and init Project ConfigurationParser

---
+++ ConfigurationParser/ConfigurationParser/ConfigParser.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma_once
+
+#include <string>
+
+class ConfigParser
+{
+public:
+	ConfigParser();
+	~ConfigParser();
+
+	struct Color{
+		float r;
+		float g;
+		float b;
+	};
+
+private:
+	float spinning;
+	float spinSpeed;
+	Color backgroundColor;
+	std::string terrainPath;
+	float terrainWidth;
+	float terrainDepth;
+	float terrainHeight;
+};
+
